3e2e18f22f0249cbf18c2101eedb8b2d95a5ba70
IEMDomain04.dc <emmanman0011@gmail.com>
2024-09-30 13:14:28 +0800

    Updated Code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { generateTextAndImage } from "./utils.js"
 
 // 1. Change the value of the variable to your name
-let name = "Roman Exequiel"
+let name = "Emman Manduriaga"
 
 // 2. Change the value of the variable to your favorite activity
-let favoriteActivity = "Racism"
+let favoriteActivity = "Design and Programming"
 
 // 3. Assign the favoritePlace variable your favorite place
 // I.e. city, mountain, pub, forrest, beach, Manhattan, etc.
-let favoritePlace = "Bar"
+let favoritePlace = "Room"
 
 // 4. Configure the AI by setting a temperature from 0 to 1
 // The higher temperature, the more random & experimental output
